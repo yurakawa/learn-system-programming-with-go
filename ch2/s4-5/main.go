3a4ad3343c5fa77caabebcdd481a43bbe575a8e0
y.urakawa <s08a2015@gmail.com>
2018-01-25 13:09:59 +0900

    [p27] gzip圧縮する

--- ch2/s4-5/main.go
+++ ch2/s4-5/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"io"
+	"compress/gzip"
 )
 
 func main() {
@@ -12,4 +13,14 @@ func main() {
 	}
 	writer := io.MultiWriter(file, os.Stdout)
 	io.WriteString(writer, "io.MultiWriter example\n")
+
+	// gzip圧縮(書き込まれたデータをgzip圧縮して、予め渡されていたos.Fileに中継する)
+	file2, err2 := os.Create("test.txt.gz")
+	if err != nil {
+		panic(err2)
+	}
+	writer2 := gzip.NewWriter(file2)
+	writer2.Header.Name = "text.txt"
+	io.WriteString(writer, "gzip.Writer example\n")
+	writer2.Close()
 }
