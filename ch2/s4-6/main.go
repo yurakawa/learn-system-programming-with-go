e4bca12c9a3469ef02c700cb697d1901cff30ccb
y.urakawa <s08a2015@gmail.com>
2018-01-27 19:06:45 +0900

    [p29] json出力を追加

--- ch2/s4-6/main.go
+++ ch2/s4-6/main.go
@@ -4,9 +4,18 @@ import (
 	"os"
 	"fmt"
 	"time"
+	"encoding/json"
 )
 
 func main() {
 	// %vはなんでも表示できるフォーマット指定子
-	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %v", time.Now())
+	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %v\n", time.Now())
+
+	// JSONを整形して出力する
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("","      ")
+	encoder.Encode(map[string]string{
+		"example": "encoding/json",
+		"hello": "world",
+	})
 }
