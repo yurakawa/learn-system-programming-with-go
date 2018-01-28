592e278c5d35025243547a8a63d590c64d31c372
y.urakawa <s08a2015@gmail.com>
2018-01-28 19:38:42 +0900

    [p42] ファイルをコピーする処理に変更

--- ch3/s3-4-2/main.go
+++ ch3/s3-4-2/main.go
@@ -8,12 +8,21 @@ import(
 // io.Readerを満たす構造体で、よくつかうもの: ファイル入力 [ os.File ]
 
 func main() {
-	file, err := os.Open("main.go")
+	srcFile, err := os.Open("main.go")
 	if err != nil {
 		panic(err)
 	}
 	// defer で現在のスコープが終了したらファイルをクローズする
-	defer file.Close()
-	io.Copy(os.Stdout, file)
+	defer srcFile.Close()
 
+	destFile, err := os.Create("wasCopied.go")
+	if err != nil {
+		panic(err)
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		panic(err)
+	}
 }
