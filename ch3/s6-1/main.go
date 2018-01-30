687b4c4d4a59bbc8691f82debb923387b18eadd9
y.urakawa <s08a2015@gmail.com>
2018-01-30 09:30:09 +0900

    [p51] デリミターを変更し単語区切りにする

--- ch3/s6-1/main.go
+++ ch3/s6-1/main.go
@@ -6,14 +6,14 @@ import (
 	"strings"
 )
 
-var source = `1行目
-2行目
-3行目`
+var source = `1行目 2行目 3行目`
 
 // テキスト解析用のio.Reader関連機能
 // bufio.Scannerを使う(デリミターは削除される)
 func main() {
 	scanner := bufio.NewScanner(strings.NewReader(source))
+	// 単語区切りに変更
+	scanner.Split(bufio.ScanWords)
 	for scanner.Scan(){
 		fmt.Printf("%#v\n",scanner.Text())
 	}
