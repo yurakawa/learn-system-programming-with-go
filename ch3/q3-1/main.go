1ea8458277d1b38b8726349ac79efa18c5b5ea4d
y.urakawa <s08a2015@gmail.com>
2018-01-30 13:52:39 +0900

    コマンドラインオプションを追加

--- ch3/q3-1/main.go
+++ ch3/q3-1/main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"os"
 	"io"
+	"flag"
+)
+var (
+	stringOpt= flag.String("s", "[-s オプションで追加した文字列だよ!]", "help message for b option" )
 )
 
+//  go run main.go old.txt new.txt.txt -s
 func main() {
-	file, err := os.Open("old.txt")
+
+	src := os.Args[1]
+	dest := os.Args[2]
+
+	file, err := os.Open(src)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	newFile, _ := os.Create("new.txt")
+	newFile, _ := os.Create(dest)
 	defer newFile.Close()
 	io.Copy(newFile, file)
-}
 
+	flag.Parse()
+	newFile.Write([]byte(*stringOpt))
+}
