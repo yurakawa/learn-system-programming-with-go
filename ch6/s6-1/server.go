eb9063495796678a756308eccbcdfaa389262a2b
y.urakawa <s08a2015@gmail.com>
2018-02-06 23:54:07 +0900

    [p108] Keep-Alive対応のHTTPサーバ

--- ch6/s6-1/server.go
+++ ch6/s6-1/server.go
@@ -8,8 +8,11 @@ import (
     "net/http"
     "net/http/httputil"
     "strings"
+    "time"
+    "io"
 )
 
+// Keep-Alive対応のHTTPサーバ
 func main () {
     listener, err := net.Listen("tcp", "localhost:8888")
     if err != nil {
@@ -22,29 +25,46 @@ func main () {
             panic(err)
         }
         go func() {
-
+            defer conn.Close()
             fmt.Printf("Accept %v\n", conn.RemoteAddr())
-            //  リクエストを読み込む
-            request, err := http.ReadRequest(
-                bufio.NewReader(conn))
-            if err != nil {
-                panic(err)
+            // Accept後のソケットで何度も応答を返すためループ
+            for {
+                // タイムアウトを設定
+                conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+                //  リクエストを読み込む
+                request, err := http.ReadRequest(bufio.NewReader(conn))
+                if err != nil {
+                    // タイムアウトもしくはソケットクローズ時は終了
+                    // それ以外はエラーになる
+                    neterr, ok := err.(net.Error)
+                    if ok && neterr.Timeout() {
+                        fmt.Println("Timeout")
+                        break
+                    } else if err == io.EOF {
+                        break
+                    }
+                    panic(err)
+                }
+
+                // リクエストを表示
+                dump, err := httputil.DumpRequest(request, true)
+                if err != nil{
+                    panic(err)
+                }
+                fmt.Println(string(dump))
+                content := "Hello World\n"
+
+                // レスポンスを書き込む
+                // HTT/1.1 かつ、ContentLengthの設定が必要
+                response := http.Response{
+                    StatusCode: 200,
+                    ProtoMajor: 1,
+                    ProtoMinor: 0,
+                    Body:       ioutil.NopCloser(
+                                strings.NewReader(content)),
+                }
+                response.Write(conn)
             }
-            dump, err := httputil.DumpRequest(request, true)
-            if err != nil{
-                panic(err)
-            }
-            fmt.Println(string(dump))
-            // レスポンスを書き込む
-            response := http.Response{
-                StatusCode: 200,
-                ProtoMajor: 1,
-                ProtoMinor: 0,
-                Body:       ioutil.NopCloser(
-                    strings.NewReader("Hello World\n")),
-            }
-            response.Write(conn)
-            conn.Close()
         }()
     }
 }
