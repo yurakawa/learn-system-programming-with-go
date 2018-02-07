81967954add30e6674ac9be425fe9a565d4741bd
y.urakawa <s08a2015@gmail.com>
2018-02-07 13:56:54 +0900

    [p114]チャンク形式のサーバの実装

--- ch6/s8-1/server.go
+++ ch6/s8-1/server.go
@@ -3,40 +3,34 @@ package main
 import (
     "bufio"
     "fmt"
-    "io/ioutil"
     "net"
     "net/http"
     "net/http/httputil"
     "strings"
-    "time"
     "io"
-    "bytes"
-    "compress/gzip"
 )
 
-// クライアントはgzipを受け入れ可能か
-func isGzipAcceptable(request *http.Request) bool {
-    return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","),"gzip")  != -1
+// 青空文庫: ごんぎつねより
+// http://www.aozora.gr.jp/cards/000121/card628.html
+var contents = []string{
+    "これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
+    "むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
+    "中山さまというおとのさまが、おられたそうです。",
+    "その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
+    "ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
+    "そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
 }
 
-// 1セッションの胥吏をする
+
 func processSession(conn net.Conn) {
     fmt.Printf("Accept %v\n", conn.RemoteAddr())
     defer conn.Close()
     // Accept後のソケットで何度も応答を返すためループ
     for {
-        // タイムアウトを設定
-        conn.SetReadDeadline(time.Now().Add(5 * time.Second))
         // リクエストを読み込む
         request, err := http.ReadRequest(bufio.NewReader(conn))
         if err != nil {
-            // タイムアウトもしくはソケットクローズ時は終了
-            // それ以外はエラーになる
-            neterr, ok := err.(net.Error)
-            if ok && neterr.Timeout() {
-                fmt.Println("Timeout")
-                break
-            } else if err == io.EOF {
+            if err == io.EOF {
                 break
             }
             panic(err)
@@ -50,23 +44,17 @@ func processSession(conn net.Conn) {
         fmt.Println(string(dump))
 
         // レスポンスを書き込む
-        response := http.Response{
-            StatusCode: 200,
-            ProtoMajor: 1,
-            ProtoMinor: 1,
-            Header: make(http.Header),
+        fmt.Fprintf(conn, strings.Join([]string{
+            "HTTP/1.1 200 OK",
+            "Content-Type: text/plain",
+            "Transfer-Encoding: chunked",
+            "", "",
+        }, "\r\n"))
+        for _, content := range contents {
+            bytes := []byte(content)
+            fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bytes), content)
         }
-        if isGzipAcceptable(request) {
-            content := "Hello World(gzipped)\n"
-            // コンテンツをgzip化して転送
-            var buffer bytes.Buffer
-            writer := gzip.NewWriter(&buffer)
-            io.WriteString(writer, content)
-            writer.Close()
-            response.Body = ioutil.NopCloser(&buffer)
-            response.ContentLength = int64(len(content))
-        }
-        response.Write(conn)
+        fmt.Fprintf(conn, "0\r\n\r\n")
     }
 }
 
