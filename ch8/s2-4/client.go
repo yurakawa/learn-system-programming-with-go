e4760bf11f40e6c4fd270a2d4812faf367e974a2
y.urakawa <s08a2015@gmail.com>
2018-02-11 02:19:03 +0900

    [p144] net.Dial()で開いたソケットは一方的な送信用でアドレス(ソケットファイルのパスと結び付けられていないので、返信を受けられないため ListenPacket を使う必要がある net.PacketConnインタフェースに変わったのでWriteToとReadFromを使う

--- ch8/s2-4/client.go
+++ ch8/s2-4/client.go
@@ -5,23 +5,34 @@ import (
     "net"
     "path/filepath"
     "os"
+    "log"
 )
 
 // データグラム型のUnixドメインソケット(クライアント)
 func main() {
-    conn, err := net.Dial("unixgram", filepath.Join(os.TempDir(), "unixdomainsocket-server"))
+    clientPath := filepath.Join(os.TempDir(), "unixdomainsocket-server")
+    os.Remove(clientPath)
+    // net.Dial()で開いたソケットは一方的な送信用でアドレス(ソケットファイルのパスと結び付けられていないので、返信を受けられないため ListenPacket を使う必要がある
+    conn, err := net.ListenPacket("unixgram", clientPath)
+    if err != nil {
+        panic(err)
+    }
+    // 送信先のアドレス
+    unixServerAddr, err := net.ResolveUnixAddr("unixgram", filepath.Join(os.TempDir(), "unixdomainsocket-server"))
+    var serverAddr net.Addr =  unixServerAddr
     if err != nil {
         panic(err)
     }
     defer conn.Close()
-    fmt.Println("Sending to server")
-    _, err = conn.Write([]byte("Hello from Client"))
+    log.Println("Sending to server")
+
+    _, err = conn.WriteTo([]byte("Hello from Client"), serverAddr)
     if err != nil {
         panic(err)
     }
     fmt.Println("Receiveing from server")
     buffer := make([]byte, 1500)
-    length, err := conn.Read(buffer)
+    length,  _, err := conn.ReadFrom(buffer)
     if err != nil {
         panic(err)
     }
