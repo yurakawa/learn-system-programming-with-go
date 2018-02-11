af96f62be93051e45f76a886ee0751319fddd28c
y.urakawa <s08a2015@gmail.com>
2018-02-12 00:14:49 +0900

    [p157] ファイルの削除

---
+++ ch9/s2-3/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "os"
+    "io"
+)
+
+func open(filename string) {
+    file, err := os.Create(filename)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    text := `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum.
+`
+    io.WriteString(file, text)
+
+}
+
+func main() {
+    filename := "server.log"
+    open(filename)
+
+    ////
+    // ファイルの削除
+
+    // ファイルや空のディレクトリの削除
+    os.Remove(filename)
+
+    os.MkdirAll("workdir/myapp/", 0755)
+    // ディレクトリを中身ごと削除
+    os.RemoveAll("workdir")
+}
