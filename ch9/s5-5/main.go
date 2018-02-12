3cdc631f6a5e60d744f6ca941f298128066e0b45
y.urakawa <s08a2015@gmail.com>
2018-02-12 17:53:26 +0900

    [p169]~ も観葉変数も展開した上でパスをクリーン化する

--- ch9/s5-5/main.go
+++ ch9/s5-5/main.go
@@ -4,19 +4,25 @@ import (
     "os"
     "fmt"
     "os/user"
+    "path/filepath"
 )
 
-func main() {
-    // 環境変数絵を展開
-    path := os.ExpandEnv("${GOPATH}/src/github.com")
-    fmt.Println(path)
-
-    // ホームディレクトリを取得
-    my, err := user.Current()
-    if err != nil {
-        panic(err)
+// ~ も観葉変数も展開した上でパスをクリーン化する
+func Clean2(path string) string {
+    if len(path) > 1 && path[0:2] == "~/" {
+        my, err := user.Current()
+        if err != nil {
+            panic(err)
+        }
+        path = my.HomeDir + path[1:]
     }
-    fmt.Println(my.HomeDir)
+    path = os.ExpandEnv(path)
+    return filepath.Clean(path)
+}
 
-
+func main() {
+    path := os.ExpandEnv("${GOPATH}/src/github.com")
+    fmt.Println(Clean2(path))
+    path = os.ExpandEnv("~/.bashrc")
+    fmt.Println(Clean2(path))
 }
