453f3b3725b6dccf100a93eeee620b1f263d6b96
y.urakawa <s08a2015@gmail.com>
2018-02-17 16:36:54 +0900

    [p255] GCを呼ぶと追加された要素が消える

--- ch13/s7-5/main.go
+++ ch13/s7-5/main.go
@@ -3,8 +3,10 @@ package main
 import (
     "fmt"
     "sync"
+    //"runtime"
 )
 
+// sync.Pool オブジェクトのキャッシュ
 func main() {
     // Poolを作成。Newで新規作成時のコードを実装
     var count int
@@ -20,6 +22,8 @@ func main() {
     // プールが空だと新規作成
     pool.Put("manualy added: 1")
     pool.Put("manualy added: 2")
+    // GCを呼ぶと追加された要素が消える
+    // runtime.GC()
     fmt.Println(pool.Get())
     fmt.Println(pool.Get())
     fmt.Println(pool.Get()) //ここは新規作成
